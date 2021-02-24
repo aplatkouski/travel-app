d59c80452be48bd406bf1fa6eeea39dca36d147b
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-24 23:02:46 +0300

    feat: add and configure jest
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{js,jsx}', '!**/node_modules/**'],
+  coverageDirectory: './coverage/',
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|scss)$': 'identity-obj-proxy',
+
+    '^Assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^Audio/(.*)$': '<rootDir>/src/assets/audio/$1',
+    '^Components/(.*)$': '<rootDir>/src/components/$1',
+    '^Data/(.*)$': '<rootDir>/src/assets/data/$1',
+    '^Entities/(.*)$': '<rootDir>/src/entities/$1',
+    '^Images/(.*)$': '<rootDir>/src/assets/images/$1',
+    '^States/(.*)$': '<rootDir>/src/redux-states/$1',
+    '^Styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^Utils/(.*)$': '<rootDir>/src/utils/$1',
+  },
+  roots: ['<rootDir>/src'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  testMatch: ['./**/*.{spec,test}.{js,jsx}'],
+  verbose: true,
+};
