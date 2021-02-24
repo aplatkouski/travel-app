1d0b1265892b83a026a83d275813a2051c05c7ca
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-02-24 23:06:09 +0300

    feat: add bootstrap and bootswatch packages to dev dependencies
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import './index.scss';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
