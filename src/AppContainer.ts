b90f3ff1f4a0a190e7e2cdb34cabf525764f2a02
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-03-11 19:41:30 +0300

    fix: remove redundant source file
    - AppContainer.ts.
    
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/AppContainer.ts
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import countries from 'States/countries';
-import App from './App';
-
-const mapDispatchToProps = {
-  fetchCountries: countries.actions.fetchCountries,
-};
-
-export default connect(undefined, mapDispatchToProps)(App);
