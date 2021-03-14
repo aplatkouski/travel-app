5488a24b4dce15dccd82364725a6a2c59f800572
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-03-14 18:17:23 +0300

    refactor: minor changes in date-time-widget component
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/components/DateTimeWidget/DateTimeWidgetContainer.ts
+++ src/components/DateTimeWidget/DateTimeWidgetContainer.ts
@@ -3,10 +3,12 @@ import * as StateTypes from 'States/types';
 import DateTimeWidget from './DateTimeWidget';
 
 const mapStateToProps = (state: StateTypes.RootState) => ({
-  language: state.languageSelector.language,
-  timezone: state.country && state.country.country && state.country.country.timezone,
-  isLoading: state.country.isLoading,
   error: state.country.error,
+  isLoading: state.country.isLoading,
+  language: state.languageSelector.language,
+  timeZone: state.country.country
+    ? state.country.country.timezone
+    : Intl.DateTimeFormat().resolvedOptions().timeZone,
 });
 
 export default connect(mapStateToProps)(DateTimeWidget);
