abb7b1bf3e721207e330d32e527881396fcd91b5
natallia-js <fedartsova@tut.by>
2021-03-14 15:40:42 +0300

    feat: add user card

---
+++ src/components/UserCard/styles.ts
@@ -0,0 +1,25 @@
+import { createStyles, Theme } from '@material-ui/core/styles';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      maxWidth: theme.spacing(60),
+      '&:hover': {
+        color: theme.palette.secondary.main,
+      },
+    },
+    media: {
+      height: theme.spacing(30),
+      transition: 'all .5s',
+      '&:hover': {
+        transform: 'scale(1.2)',
+        backgroundPositionY: '-18px',
+      },
+    },
+    user: {
+      color: theme.palette.text.secondary,
+    },
+  });
+
+export default styles;
