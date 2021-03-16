039940942c15c615ab976feec550b64f2a24f914
Vitaly Raicheu <RVitaly1978@gmail.com>
2021-03-16 21:01:46 +0300

    feat: add maps on selected language

--- src/constants.ts
+++ src/constants.ts
@@ -14,6 +14,10 @@ export const WEATHER_API_KEY = '679070626d7fff4194b0a776c3a2f8a7';
 export const WEATHER_DATA_UPDATE_MS = 60000;
 export const MAP_API_KEY =
   'pk.eyJ1IjoicnZpdGFseSIsImEiOiJja2p4Ymx3ZDEwcW05MnVwZ2dicHlyd2kwIn0.TEXhkHWxKEzDhrScrjeumQ';
-export const MAP_API_LAYER_ID = 'mapbox/streets-v11';
-export const MAP_API_URL = `https://api.mapbox.com/styles/v1/${MAP_API_LAYER_ID}/tiles/{z}/{x}/{y}?access_token=${MAP_API_KEY}`;
+export const MAP_API_LAYER_ID_EN = 'mapbox/streets-v11';
+export const MAP_API_LAYER_ID_RU = 'rvitaly/ckmc8jskji0li17rydmvsijhq';
+export const MAP_API_LAYER_ID_DE = 'rvitaly/ckmc9ya90ko3517l9l2wppng7';
+export const MAP_API_URL_EN = `https://api.mapbox.com/styles/v1/${MAP_API_LAYER_ID_EN}/tiles/{z}/{x}/{y}?access_token=${MAP_API_KEY}`;
+export const MAP_API_URL_RU = `https://api.mapbox.com/styles/v1/${MAP_API_LAYER_ID_RU}/tiles/{z}/{x}/{y}?access_token=${MAP_API_KEY}`;
+export const MAP_API_URL_DE = `https://api.mapbox.com/styles/v1/${MAP_API_LAYER_ID_DE}/tiles/{z}/{x}/{y}?access_token=${MAP_API_KEY}`;
 export const FLAG_API_URL = 'https://restcountries.eu/data/';
