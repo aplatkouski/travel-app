da8228f872a9cdbe600a9b48be222c00bceb5557
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-03-17 02:03:27 +0300

    refactor: add i18n directory with translate.ts and en/de/ru.json
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/constants.ts
+++ src/constants.ts
@@ -3,13 +3,7 @@ export const countriesAPI = 'todos/countries';
 export const countriesPerPage = 9;
 export const countryAPI = 'todos/countryDetailed';
 export const MAX_PANEL_IMAGES = 6;
-export const SUCCESSFUL_REGISTRATION_MESSAGE = {
-  en: 'Congratulations! You have been registered successfully. Now, You can log in.',
-  ru:
-    'Поздравляем! Вы успешно зарегистрировались. Теперь Вы можете выполнить вход в систему.',
-  de:
-    'Herzliche Glückwünsche! Sie haben sich erfolgreich registriert. Sie können sich jetzt beim System anmelden.',
-};
+export const SUCCESSFUL_REGISTRATION_MESSAGE = {};
 export const USER_REGISTRATION_API = 'auth/register';
 export const GET_USER_PHOTO_API = 'auth/getUserPhoto';
 export const LOCALSTORAGE_KEY = 'travel-app-storage';
