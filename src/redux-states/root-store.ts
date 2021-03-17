93d44139ff9162d4531613682684ef294e771476
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-03-17 22:08:28 +0300

    refactor: minor changes in root store
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/redux-states/root-store.ts
+++ src/redux-states/root-store.ts
@@ -2,6 +2,7 @@ import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import loggerMiddleware from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
+import { IState as ILanguageSelectorState } from 'States/language-selector/model';
 import { rootReducer } from './root-reducer';
 
 const composedEnhancer = composeWithDevTools({
@@ -9,13 +10,22 @@ const composedEnhancer = composeWithDevTools({
   traceLimit: 25,
 });
 
+const preloadedLanguage = {
+  languageSelector: localStorage.languageSelector
+    ? (JSON.parse(localStorage.languageSelector) as ILanguageSelectorState)
+    : undefined,
+};
+
 const store = createStore(
   rootReducer,
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  preloadedLanguage,
   composedEnhancer(applyMiddleware(loggerMiddleware, thunkMiddleware))
 );
 
 store.subscribe(() => {
-  localStorage.setItem('travel-app', JSON.stringify(store.getState().languageSelector));
+  localStorage.languageSelector = JSON.stringify(store.getState().languageSelector);
 });
 
 export default store;
